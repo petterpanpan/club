fe67d23e8961a4680d7e630c2ffc7da6727c22c8
pan <1400778647@qq.com>
2020-07-21 20:17:18 +0800

    1
    1

---
+++ baogao/itemlink.js
@@ -0,0 +1,69 @@
+﻿
+var xItemIDs={};
+
+var xBkmkIDs={};
+
+$(document).ready(function(){
+
+	var _urlTarget=function(sHref){
+		var sUrl;
+		var m=sHref.match(/^nyf:\/\/entry\?itemid=(\d+)/i);
+		if(m && m.length>1){
+			var sFn=xItemIDs[m[1]];
+			if(sFn){
+				sUrl=sFn;
+			}else{
+				alert('Cannot locate the target webpage. \n\n' + sHref);
+			}
+			if(!sUrl) sUrl='';
+		}else{
+			m=sHref.match(/^nyf:\/\/entry\?bmid=(\d+)/i);
+			if(m && m.length>1){
+				var sBmID=m[1];
+				if(sBmID){
+					var v=(xBkmkIDs[sBmID]||'').split('\t');
+					if(v && v.length>2){
+						var sItemID=parseInt(v[0]), sSsgName=v[1], sAnchor=v[2];
+						var sFn=xItemIDs[sItemID];
+						if(sFn){
+							sUrl=sFn + '#' + sAnchor;
+						}else{
+							alert('Cannot locate the target webpage. \n\n' + sHref);
+						}
+					}
+				}
+				if(!sUrl) sUrl='';
+			}
+		}
+		return sUrl;
+	};
+
+	var linkOf=function(p){
+		var sUri='';
+		while(p && !sUri){
+			sUri=p.href||'';
+			p=p.parentNode;
+		}
+		return sUri;
+	};
+
+	$('a').click(function(e){
+		//2018.4.5 the target element could be a <SPAN> inside of <a>
+		var sHref=linkOf(e.target);
+		if(sHref){
+			var sUrl=_urlTarget(sHref);
+			if(sUrl){
+				document.location.href=sUrl;
+				return false;
+			}else if(sUrl===''){
+				return false; //2017.10.21 bad nyf:// links;
+			}else{
+				window.open(sHref);
+				return false;
+			}
+		}else{
+			return false;
+		}
+	});
+
+});
